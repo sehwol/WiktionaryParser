be10cdec6bbc82d03dc69c5b5ee3c2e2cc114dd8
sehwol <sehwol@users.noreply.github.com>
2020-09-05 00:50:19 +0700

    test: fix tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,39 +1,21 @@
 from parameterized import parameterized
 import unittest
 import json
-from wiktionaryparser import WiktionaryParser
+from .context import WiktionaryParser
 from deepdiff import DeepDiff
 from typing import Dict
-import os
 
 parser = WiktionaryParser()
-
-current_dir = os.path.dirname(__file__)
-markdown_files_dir = os.path.join(current_dir, 'markdown_files')
-
 
 
 class TestParser(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         self.expected_results = {}
+
         with open('tests/testOutput.json', 'r') as f:
             self.expected_results = json.load(f)
 
         super(TestParser, self).__init__(*args, **kwargs)
-
-    def test_fetch_wikitext(self):
-        expected = "{{also|nööp|no-op}}\n==Dutch==\n\n===Pronunciation===\n* {{audio|nl|Nl-noop.ogg|Audio}}\n\n===Verb===\n{{head|nl|verb form}}\n\n# {{nl-verb form of|p=1|n=sg|t=pres|m=ind|nopen}}\n# {{nl-verb form of|m=imp|nopen}}"
-        result = parser.fetch_wikitext('noop')
-
-        self.assertEqual(expected, result)
-
-    @parameterized.expand([
-        ('Swedish', 'house', 50356446)
-    ])
-    def test_parse_wikitext(self, language: str, word: str, old_id: int):
-        wikitext = self.__get_test_wikitext(word, old_id)
-        word_dto = parser.parse_wikitext(wikitext, word, language)
-        self.__test_word_dto(word_dto, word, language)
 
     @parameterized.expand([
         ('grapple', 50080840),
@@ -85,30 +67,24 @@ class TestParser(unittest.TestCase):
 
     def __test_word(self, word: str, old_id: int, lang: str):
         parser.set_default_language(lang)
-        word_dto = parser.fetch(word, old_id=old_id)
+        fetched_word = parser.fetch(word, old_id=old_id)
 
-        self.__test_word_dto(word_dto, word, lang)
-
-    def __test_word_dto(self, word_dto, word: str, lang: str):
         print("Testing \"{}\" in \"{}\"".format(word, lang))
         expected_result = self.expected_results[lang][word]
 
-        diff = DeepDiff(word_dto,
+        diff = DeepDiff(fetched_word,
                         expected_result,
                         ignore_order=True)
 
         if diff != {}:
             print("Found mismatch in \"{}\" in \"{}\"".format(word, lang))
             print(json.dumps(json.loads(diff.json), indent=4))
+            print("Actual result:")
+            print(json.dumps(fetched_word, indent=4))
 
         self.assertEqual(diff, {})
-
-    def __get_test_wikitext(self, page_name: str, old_id: int) -> str:
-        file_path = os.path.join(markdown_files_dir, '{}-{}.md'.format(page_name, old_id))
-
-        with open(file_path, 'r', encoding='utf-8') as f:
-            return f.read()
 
 
 if __name__ == '__main__':
     unittest.main()
+ 	
